0c7270dfafd3b7e95ea9cc5a1348b2caf71fa915
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-14 21:58:33 +0200

    add: Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import Modal from "./components/UI/Modal";
 import { CartContextProvider } from "./store/CartContext";
 
 function App() {
   return (
     <CartContextProvider>
+      <Modal open>Modal</Modal>
       <Header />
       <main>
         <Cards />
