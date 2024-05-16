a493ce63b5f3e0f3788c777e522ec67814673814
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-16 20:10:57 +0200

    Adding a Custom HTTP Hook & Avoiding Common Errors

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,32 +1,22 @@
-import { useEffect, useState } from "react";
-import { getAllMeals } from "../http";
 import Card from "./Card";
+import useHttp from "../hook/useHttp";
+
+const requestConfig = {};
 
 export default function Cards() {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const {
+    data: meals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    setIsLoading(true);
-    async function fetchMeals() {
-      try {
-        const meals = await getAllMeals();
-        setMeals(meals);
-      } catch (error) {
-        setIsError({
-          message:
-            error.message || "Could not fetch places, please try again later.",
-        });
-      }
-      setIsLoading(false);
-    }
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
+
   return (
     <section>
-      {isLoading && <p>Loadong...</p>}
-      {isError && <p>Error</p>}
+      {/* {error && <p>Error</p>} */}
       <ul id="meals">
         {meals.map((meal) => (
           <Card key={meal.id} meal={meal} />
