150763a122229884fa47d79cd8233c633b87070a
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-16 10:25:07 +0200

    add Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Cards from "./components/Cards";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 import Header from "./components/Header";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
@@ -9,6 +10,7 @@ function App() {
     <UserProgressContextProvider>
       <CartContextProvider>
         <Cart />
+        <Checkout />
         <Header />
         <main>
           <Cards />
