d6e3c97e15bde531d08abdf2177e011caf328b8d
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-17 10:36:30 +0200

    commit

--- src/hook/useHttp.js
+++ src/hook/useHttp.js
@@ -10,7 +10,6 @@ async function sendHttpRequest(url, config) {
   }
   return resData;
 }
-mklo;
 export default function useHttp(url, config, initialData) {
   const [data, setData] = useState(initialData);
   const [isLoading, setIsLoading] = useState(false);
